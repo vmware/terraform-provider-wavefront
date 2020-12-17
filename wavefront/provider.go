363d79c9d34bbde43a3ed5dff493af63a9d2a2ea
Robert Fisher <services@id264.net>
2020-12-17 14:52:00 +0000

    add ingestion policy support

--- wavefront/provider.go
+++ wavefront/provider.go
@@ -48,6 +48,7 @@ func Provider() *schema.Provider {
 			"wavefront_dashboard_json":                       resourceDashboardJSON(),
 			"wavefront_derived_metric":                       resourceDerivedMetric(),
 			"wavefront_external_link":                        resourceExternalLink(),
+			"wavefront_ingestion_policy":                     resourceIngestionPolicy(),
 			"wavefront_maintenance_window":                   resourceMaintenanceWindow(),
 			"wavefront_service_account":                      resourceServiceAccount(),
 			"wavefront_role":                                 resourceRole(),
