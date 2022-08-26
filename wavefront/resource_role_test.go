927552f060062ecbc52ec14f06d1cd981eed213c
dkuldeep22 <dkuldeep@vmware.com>
2022-08-26 15:56:33 +0530

    Upgrading terraform-sdkv2-plugin to 2.21.0 and go version to 1.17; fixing test cases

--- wavefront/resource_role_test.go
+++ wavefront/resource_role_test.go
@@ -56,7 +56,7 @@ func TestAccWavefrontRole_AdvancedRole(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "name", "Test Role"),
 					resource.TestCheckResourceAttr(resourceName, "description", "Test Role Description"),
 					resource.TestCheckResourceAttr(resourceName, "permissions.#", "2"),
-					resource.TestCheckNoResourceAttr(resourceName, "assignees"),
+					resource.TestCheckResourceAttr(resourceName, "assignees.#", "0"),
 				),
 			},
 		},
