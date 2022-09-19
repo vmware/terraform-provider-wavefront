96ba22eea534083472b8973b8e94e20f940dc0e1
dkuldeep22 <dkuldeep@vmware.com>
2022-09-19 23:00:22 +0530

    fixing terraform provider acceptance tests

--- wavefront/import_user_test.go
+++ wavefront/import_user_test.go
@@ -1,8 +1,6 @@
 package wavefront
 
 import (
-	"os"
-	"strings"
 	"testing"
 
 	"github.com/WavefrontHQ/go-wavefront-management-api"
@@ -13,24 +11,17 @@ func TestAccUser_Basic(t *testing.T) {
 	resourceName := "wavefront_user.basic"
 	var record wavefront.User
 
-	config := testAccCheckWavefrontUserImporterBasic()
-	if os.Getenv("TF_ACC") == "1" {
-		replace := "tftesting"
-		newCustomer := getCustomerName()
-		config = strings.Replace(config, replace, newCustomer, 1)
-	}
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckWavefrontUserDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: config,
+				Config: testAccCheckWavefrontUserImporterBasic(),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckWavefrontUserExists("wavefront_user.basic", &record),
 				),
-			},
-			{
+			}, {
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
@@ -40,11 +31,12 @@ func TestAccUser_Basic(t *testing.T) {
 }
 
 func testAccCheckWavefrontUserImporterBasic() string {
-	return `resource "wavefront_user" "basic" {
-	       email  = "test+tftesting@example.com"
-	       permissions = [
-	               "agent_management",
-	               "alerts_management",
-	       ]
-	}`
+	return `
+resource "wavefront_user" "basic" {
+	email  = "test+tftesting@example.com"
+	permissions = [
+		"agent_management",
+		"alerts_management",
+	]
+}`
 }
