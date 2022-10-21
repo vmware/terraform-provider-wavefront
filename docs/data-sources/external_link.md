148ec194847f6bca8c496da4a994602cde94f34e
Kuldeep Doneriya <101704094+dkuldeep22@users.noreply.github.com>
2022-10-21 13:56:32 +0530

    Adding Maintenance Window & External Links Data sources (#100)
    * Adding Maintenance Window & External Links Data sources

---
+++ docs/data-sources/external_link.md
@@ -0,0 +1,40 @@
+---
+layout: "wavefront"
+page_title: "Wavefront: External Link"
+description: |-
+    Get the information about a specific Wavefront external link.
+---
+
+# Data Source: wavefront_external_link
+
+Use this data source to get information about a Wavefront external link by its ID.
+
+## Argument Reference
+* `id` - (Required) The ID of the external link.
+
+## Example Usage
+
+```hcl
+# Get the information about a specific external links.
+data "wavefront_external_link" "example" {
+  id = "sample-external-link-id"
+}
+```
+
+## Attribute Reference
+
+* `name` - The name of the external link.
+* `id` -  The ID of the external link.
+* `description` - Human-readable description of this link.
+* `template` - The mustache template for the link. The template must expand to a full URL, including scheme, origin, etc.
+* `metric_filter_regex` - Controls whether a link is displayed in the context menu of a highlighted series. If present, the metric name of the highlighted series must match this regular expression in order for the link to be displayed.
+* `source_filter_regex` - Controls whether a link is displayed in the context menu of a highlighted series. If present, the source name of the highlighted series must match this regular expression in order for the link to be displayed.
+* `point_tag_filter_regexes` - (Optional) Controls whether a link is displayed in the context menu of a highlighted
+      series. This is a map from string to regular expression. The highlighted series must contain point tags whose
+      keys are present in the keys of this map and whose values match the regular expressions associated with those
+      keys in order for the link to be displayed.
+* `is_log_integration` - Whether this is a "Log Integration" subType of external link.
+* `created_epoch_millis` - The timestamp in epoch milliseconds indicating when the external link is created.
+* `updated_epoch_millis` - The timestamp in epoch milliseconds indicating when the external link is updated.
+* `creator_id` - The ID of the user who created the external link.
+* `updater_id` - The ID of the user who updated the external link.
---
+++ wavefront/data_source_maintenance_window_all.go
@@ -0,0 +1,92 @@
+package wavefront
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/WavefrontHQ/go-wavefront-management-api"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceMaintenanceWindows() *schema.Resource {
+	return &schema.Resource{
+		Read:   dataSourceMaintenanceWindowsRead,
+		Schema: dataSourceMaintenanceWindowsSchema(),
+	}
+}
+
+func dataSourceMaintenanceWindowsSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		// Computed Values
+		maintenanceWindowsKey: {
+			Type:     schema.TypeSet,
+			Computed: true,
+			Elem: &schema.Resource{
+				Schema: dataSourceMaintenanceWindowSchema(),
+			},
+		},
+		limitKey: {
+			Type:     schema.TypeInt,
+			Optional: true,
+			Default:  100,
+		},
+		offsetKey: {
+			Type:     schema.TypeInt,
+			Optional: true,
+			Default:  0,
+		},
+	}
+
+}
+
+func dataSourceMaintenanceWindowsRead(d *schema.ResourceData, m interface{}) error {
+	var allMaintenanceWindows []*wavefront.MaintenanceWindow
+
+	limit := d.Get(limitKey).(int)
+	offset := d.Get(offsetKey).(int)
+
+	if err := json.Unmarshal(searchAll(limit, offset, "maintenancewindow", nil, nil, m), &allMaintenanceWindows); err != nil {
+		return fmt.Errorf("Response is invalid JSON")
+	}
+
+	// Data Source ID is set to current time to always refresh
+	d.SetId(time.Now().UTC().String())
+
+	if err := d.Set(maintenanceWindowsKey, flattenMaintenanceWindows(allMaintenanceWindows)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func flattenMaintenanceWindows(maintenanceWindows []*wavefront.MaintenanceWindow) []map[string]interface{} {
+	tfMaps := make([]map[string]interface{}, len(maintenanceWindows))
+	for i, v := range maintenanceWindows {
+		tfMaps[i] = flattenMaintenanceWindow(v)
+	}
+	return tfMaps
+}
+
+func flattenMaintenanceWindow(maintenanceWindow *wavefront.MaintenanceWindow) map[string]interface{} {
+	tfMap := make(map[string]interface{})
+	tfMap[idKey] = maintenanceWindow.ID
+	tfMap[eventNameKey] = maintenanceWindow.EventName
+	tfMap[runningStateKey] = maintenanceWindow.RunningState
+	tfMap[reasonKey] = maintenanceWindow.Reason
+	tfMap[sortAttrKey] = maintenanceWindow.SortAttr
+	tfMap[createdEpochMillisKey] = maintenanceWindow.CreatedEpochMillis
+	tfMap[updatedEpochMillisKey] = maintenanceWindow.UpdatedEpochMillis
+	tfMap[customerIDKey] = maintenanceWindow.CustomerId
+	tfMap[titleKey] = maintenanceWindow.Title
+	tfMap[startTimeInSecondsKey] = maintenanceWindow.StartTimeInSeconds
+	tfMap[endTimeInSecondsKey] = maintenanceWindow.EndTimeInSeconds
+	tfMap[creatorIDKey] = maintenanceWindow.CreatorId
+	tfMap[updaterIDKey] = maintenanceWindow.UpdaterId
+	tfMap[relevantHostTagsAndedKey] = maintenanceWindow.RelevantHostTagsAnded
+	tfMap[hostTagGroupHostNamesGroupAndedKey] = maintenanceWindow.HostTagGroupHostNamesGroupAnded
+	tfMap[relevantCustomerTagsKey] = maintenanceWindow.RelevantCustomerTags
+	tfMap[relevantHostNamesKey] = maintenanceWindow.RelevantHostNames
+	tfMap[relevantHostTagsKey] = maintenanceWindow.RelevantHostTags
+
+	return tfMap
+}
